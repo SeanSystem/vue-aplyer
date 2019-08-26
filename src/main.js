1444eea178c0742ba2e13397fd909b37eb42f76f
sean <1403273045@qq.com>
2019-08-26 16:35:02 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import APlayer from '@moefe/vue-aplayer';
+
+Vue.use(APlayer, {
+  defaultCover: 'https://github.com/u3u.png',
+  productionTip: true,
+});
 
 Vue.config.productionTip = false
 
