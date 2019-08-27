90079637e702e508f413ef0788c84e023fa34369
sean <1403273045@qq.com>
2019-08-27 13:49:50 +0800

    新增动态获取歌曲资源

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hello">
-        <aplayer ref="aplayer" :audio="audio" :lrcType="3" autoplay/> <!--fixed 吸底模式 mini 迷你模式-->
+        <aplayer ref="aplayer" :audio="audio" :lrcType="3" autoplay fixed/> <!--fixed 吸底模式 mini 迷你模式-->
     </div>
 </template>
 
@@ -9,31 +9,8 @@
         name: 'HelloWorld',
         data() {
             return {
-                musicUrl: "localhost:8090/hugeMusic/search",
-                audio: [
-                    {
-                        name: '东西（Cover：林俊呈）',
-                        artist: '纳豆',
-                        url: 'https://cdn.moefe.org/music/mp3/thing.mp3',
-                        //  cover: 'https://p1.music.126.net/5zs7IvmLv7KahY3BFzUmrg==/109951163635241613.jpg?param=300y300', // prettier-ignore
-                        // lrc: 'https://cdn.moefe.org/music/lrc/thing.lrc',
-                    },
-                    {
-                        name: '响喜乱舞（Cover：MARiA）',
-                        artist: '泠鸢yousa',
-                        url: 'https://cdn.moefe.org/music/mp3/kyoukiranbu.mp3',
-                        // cover: 'https://p1.music.126.net/AUGVPQ_rVrngDH9ocQrn3Q==/109951163613037822.jpg?param=300y300', // prettier-ignore
-                        //  lrc: 'https://cdn.moefe.org/music/lrc/kyoukiranbu.lrc',
-                    },
-                    {
-                        name: '啵唧',
-                        artist: 'Hanser',
-                        url: 'https://cdn.moefe.org/music/mp3/kiss.mp3',
-                        // cover: 'https://p1.music.126.net/K0-IPcIQ9QFvA0jXTBqoWQ==/109951163636756693.jpg?param=300y300', // prettier-ignore
-                        //   lrc: 'https://cdn.moefe.org/music/lrc/kiss.lrc',
-                    },
-                ]
-                ,
+                musicUrl: "http://localhost:8090/hugeMusic/searchAplayer",
+                audio: [],
             };
         },
         mounted: function () {
@@ -47,9 +24,7 @@
                 }
             )
                 .then((res) => {
-                    var musicData = res.data;
-                    // eslint-disable-next-line no-console
-                    console.log(musicData);
+                    this.audio = res.data;
                 })
                 .catch();
         }
